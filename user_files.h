317e77c30e1ea097d467131943e2d05b14b66493
tsatsulya <dmirtytsatsulin@gmail.com>
2023-05-17 20:27:51 +0300

    feature: added header guards for USER_FILE_READER

--- user_files.h
+++ user_files.h
@@ -1,11 +1,14 @@
-#pragma once
+#ifndef USER_FILES_READER_H
+#define USER_FILES_READER_H
 
 #include <boost/filesystem.hpp>
 #include "audio_data_decoder/audio_data.h"
 #include "directories.h"
 namespace directory_scaner {
-	enum reading_mod : char {RECURSIVE = 0, LINEAL};
+	enum reading_mod : char {RECURSIVE = 0, LINEAR};
 
 	static directories::directory *directory_content = new directories::directory();
-	directories::directory *read_directory(const char *path_to_root_directory = "/", reading_mod mod = LINEAL, directories::directory *dir = directory_content);
+	directories::directory *read_directory(const char *path_to_root_directory = "/", reading_mod mod = LINEAR, directories::directory *dir = directory_content);
 }
+
+#endif
